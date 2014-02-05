e8715a1cfaaf291b1a91b6d9d5969c65e09d8fe5
Jeff Casimir <jeff@casimircreative.com>
2014-02-05 10:06:07 -0700

    The one-liner

--- subarray.rb
+++ subarray.rb
@@ -1,25 +1,10 @@
 class Subarray
+
   def self.greatest_from(input)
-    largest = input.first
-
-    input.each do |data|
-      if sum(data) > sum(largest)
-        largest = data
-      end
-    end
-
-    return largest
+    input.max_by{|data| data.reduce(:+) }
   end
 
   def self.sum(input)
     input.reduce(:+)
-
-    #input.reduce(0){|sum, data| sum + data}
-
-    # total = 0
-    # input.each do |data|
-    #   total = total + data
-    # end
-    # return total
   end
 end
